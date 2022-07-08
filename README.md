e30412e3f8d897ea7f0e8801110da9bd728f4de2
Web Developer <useraccount@WebDeveloperMBP.local>
2022-07-08 15:57:54 -0500

    Cols and Rows - LM

--- README.md
+++ README.md
@@ -1,3 +1,77 @@
 # TableData
 
-App Description
+## VTEX Block Example - Columns Only
+```
+"tabledata#kids-size-table": {
+    "props": {
+      "caption": "Kids' Bicycle Sizing Chart",
+      "columns": [
+        "Rider Height",
+        "Bike Size"
+      ],
+      "data": [
+        [
+          "42in - 50in",
+          "20in Bike"
+        ],
+        [
+          "50in - 58in",
+          "24in Bike"
+        ],
+        [
+          "58in +",
+          {
+            "text": "Adult Bike Sizes",
+            "url": "#adult-sizing",
+            "newTab": false
+          }
+        ]
+      ]
+    }
+  }
+```
+* Table renders in a `<figure>` tag.
+* Caption Prop renders above the table data in a `<figcaption>` tag.
+* Columns Prop is an array of strings containing the column headers. Number of columns is C.
+* Columns render in `<th>` tags and are bold text.
+* Data Prop is an array of arrays.
+    * Inner Array must be of length C.
+    * Array item may be a string or a link prop
+        * Link Prop is...
+        ```
+        "text": "string",
+        "url": "string",
+        "newTab: boolean
+        ```
+
+## DOM Example - Columns Only
+```
+<figure class="eriksbikeshop-tabledata-1-x-tableDataContainer">
+    <figcaption class="eriksbikeshop-tabledata-1-x-caption">
+        <div class="eriksbikeshop-tabledata-1-x-captionText">Kids' Bicycle Sizing Chart</div>
+    </figcaption>
+    <table class="eriksbikeshop-tabledata-1-x-table">
+        <thead class="eriksbikeshop-tabledata-1-x-thead">
+            <tr data-tr="0" class="eriksbikeshop-tabledata-1-x-tr">
+                <th data-th="0" style="width: 50%;" class="eriksbikeshop-tabledata-1-x-th">Rider Height</th>
+                <th data-th="1" style="width: 50%;" class="eriksbikeshop-tabledata-1-x-th">Bike Size</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr data-tr="1" class="eriksbikeshop-tabledata-1-x-tr">
+                <td class="eriksbikeshop-tabledata-1-x-td">42in - 50in</td>
+                <td class="eriksbikeshop-tabledata-1-x-td">20in Bike</td>
+            </tr>
+            <tr data-tr="2" class="eriksbikeshop-tabledata-1-x-tr">
+                <td class="eriksbikeshop-tabledata-1-x-td">50in - 58in</td>
+                <td class="eriksbikeshop-tabledata-1-x-td">24in Bike</td>
+            </tr>
+            <tr data-tr="3" class="eriksbikeshop-tabledata-1-x-tr">
+                <td class="eriksbikeshop-tabledata-1-x-td">58in +</td>
+                <td class="eriksbikeshop-tabledata-1-x-td">
+                    <a href="#adult-sizing" target="_self" class="eriksbikeshop-tabledata-1-x-tdLink">Adult Bike Sizes</a></td>
+            </tr>
+        </tbody>
+    </table>
+</figure>
+```
--- react/TableData.tsx
+++ react/TableData.tsx
@@ -1,5 +1,4 @@
-
-import React, { ReactChildren, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Styles
 import styles from "./styles.css";
@@ -14,55 +13,125 @@ interface CellObject {
   text: string
   url: string
   newTab: Boolean
+  rowTitle: string
 }
-
 
 const TableData: StorefrontFunctionComponent<TableDataProps> = ({ caption, columns, data }) => {
   const [openGate, setOpenGate] = useState<Boolean>(true);
+  const [colWidth, setColWidth] = useState<string>("");
+  const [columnsOnly, setColumnsOnly] = useState<Boolean>(false);
+  const [columnsAndRows, setColumnsAndRows] = useState<Boolean>(false);
 
   useEffect(() => {
     if (!openGate) return;
-    console.clear();
-    cleanData();
+    setOpenGate(false);
+    format();
   })
 
-  const cleanData = () => {
-    data.forEach(tr => {
-      tr.forEach(td => {
-        console.log(td);
+  const format = () => {
+    let rowObjects = 0;
+    data.forEach(point => {
+      point.forEach(row => {
+        if (typeof row === "object") {
+          if (row.rowTitle) {
+            rowObjects = rowObjects + 1;
+          }
+        }
       })
     })
+
+    if (rowObjects > 0) {
+      setColumnsAndRows(true);
+      setColWidth((100 / (columns.length + 1)) + "%");
+    } else {
+      setColumnsOnly(true);
+      setColWidth((100 / columns.length) + "%");
+    }
+  }
+
+  const handleHover = (e: any) => {
+    const activeCell = e.currentTarget;
+
+    const cellData = activeCell.dataset.cell;
+    const cellRow = cellData.split("row-")[1].split("-col")[0];
+    const cellCol = cellData.split("col-")[1];
+
+    // @ts-expect-error
+    const activeRow = document.querySelector(`[data-cell="row-${cellRow}-col-0"]`);
+
+    // @ts-expect-error
+    const activeCol = document.querySelector(`[data-th="${cellCol}"]`);
+
+    if (activeRow && activeCol) {
+      activeRow.classList.toggle("eriksbikeshop-tabledata-1-x-highlightHeader");
+      activeCol.classList.toggle("eriksbikeshop-tabledata-1-x-highlightHeader");
+    }
+
+    if (cellCol !== "0") activeCell.classList.toggle("eriksbikeshop-tabledata-1-x-highlightCell");
   }
 
   const cleanLink = (link: CellObject) => <a href={link.url} target={link.newTab ? "_blank" : "_self"} className={styles.tdLink}>{link.text}</a>;
 
-  return (
-    <figure className={styles.tableDataContainer}>
-      <figcaption className={styles.caption}>
-        <div className={styles.captionText}>{caption}</div>
-      </figcaption>
-      <table className={styles.table}>
-        <thead className={styles.thead}>
-          <tr className={styles.tr}>
-            {columns.map((col, colIndex) => (
-              <th key={colIndex} data-col={colIndex} className={styles.th}>{col}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(tr => (
-            <tr className={styles.tr} data-fake={tr}>
-              {
-                tr.map(td => (
-                  <td className={styles.td}>{typeof td === "string" ? td : cleanLink(td)}</td>
-                ))
-              }
+  if (columnsOnly) {
+    return (
+      <figure className={styles.tableDataContainer}>
+        <figcaption className={styles.caption}>
+          <div className={styles.captionText}>{caption}</div>
+        </figcaption>
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr data-tr="0" className={styles.tr}>
+              {columns.map((col, colIndex) => (
+                <th key={`col${colIndex}`} data-th={colIndex} style={{ width: colWidth }} className={styles.th}>{col}</th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </figure>
-  )
+          </thead>
+          <tbody>
+            {data.map((tr, trIndex) => (
+              <tr key={`tr${trIndex}`} data-tr={trIndex + 1} className={styles.tr}>
+                {
+                  tr.map((td, tdIndex) => (
+                    <td key={`td${tdIndex}`} className={styles.td}>{typeof td === "string" ? td : cleanLink(td)}</td>
+                  ))
+                }
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </figure>
+    )
+  } else if (columnsAndRows) {
+    return (
+      <figure className={`${styles.tableDataContainer} ${styles.hasRows}`}>
+        <figcaption className={`${styles.caption} ${styles.bottomCaption}`}>
+          <div className={`${styles.captionText} ${styles.standAloneCaption}`}>{caption}</div>
+        </figcaption>
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr data-tr="0" className={styles.tr}>
+              <th style={{ width: colWidth }} className={styles.thBlank}></th>
+              {columns.map((col, colIndex) => (
+                <th key={`col${colIndex}`} data-th={colIndex + 1} style={{ width: colWidth }} className={styles.th}>{col}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((tr, trIndex) => (
+              <tr key={`tr${trIndex}`} data-tr={trIndex + 1} className={styles.tr}>
+                {
+                  tr.map((td, tdIndex) => (
+                    <td key={`td${tdIndex}`} data-cell={`row-${trIndex + 1}-col-${tdIndex}`} onMouseOver={handleHover} onMouseOut={handleHover} className={styles.td}>{typeof td === "string" ? td : td.rowTitle ? td.rowTitle : cleanLink(td)}</td>
+                  ))
+                }
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </figure>
+    )
+  } else {
+    return <></>
+  }
 }
 
 TableData.schema = {
